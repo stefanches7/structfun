3c327edd9f8683ccf01bb9ee599d3a692f4cd9d1
Stefan Dvoretskii <stefan.dvoretskii@tum.de>
2023-06-30 11:48:08 +0200

    comment code to make it more understandable

--- hebbian_adjust.m
+++ hebbian_adjust.m
@@ -4,21 +4,21 @@ function [S] = hebbian_adjust(S, fired, rewiring_rate)
     if (length(fired) < 2) 
         return; end
     for i=1:length(fired)
-        wire_with = fired(:);
+        wire_this_to = fired(:);
         inc_weights_sum = sum(S(i, :)); % synaptic homeostasis
-        count_wire = length(fired)-1;
-        count_unwire = size(S,1) - count_wire;
-        wire_delta = rewiring_rate*inc_weights_sum/count_wire;
-        unwire_delta = rewiring_rate*inc_weights_sum/count_unwire;
+        count_wire = length(fired)-1; % no wiring with self
+        count_unwire = size(S,1) - count_wire; 
+        wire_by = rewiring_rate*inc_weights_sum/count_wire;
+        unwire_by = rewiring_rate*inc_weights_sum/count_unwire;
         for j=1:size(S, 1)
-            if ~length(wire_with)==0 && wire_with(1) == j
-                if j ==i wire_with = wire_with(2:end);
+            if ~isempty(wire_this_to) && wire_this_to(1) == j
+                if j ==i wire_this_to = wire_this_to(2:end); %skip reflective wiring
                 else 
-                    S(i,j) = S(i,j) + wire_delta;
-                    wire_with = wire_with(2:end);
+                    S(i,j) = S(i,j) + wire_by;
+                    wire_this_to = wire_this_to(2:end);
                 end
             else % not-fired connection
-                S(i,j) = S(i,j) - unwire_delta;
+                S(i,j) = S(i,j) - unwire_by;
             end
         end
     end
--- hebbian_adjust_network_homeostasis.m
+++ hebbian_adjust_network_homeostasis.m
@@ -5,17 +5,17 @@
 function [S] = hebbian_adjust_network_homeostasis(S, fired, rewiring_rate)
     if (length(fired) < 2) 
         return; end
-    inc_weights_sum = sum(S, "all"); % synaptic homeostasis
+    increased_weights_sum = sum(S, "all"); % synaptic homeostasis
     [X, Y] = meshgrid(fired, fired);
     firedxfired = [X(:) Y(:)];
-    for i=1:size(firedxfired, 1)
+    for i=1:size(firedxfired, 1) % go through all firing together pairs
         x = firedxfired(i, 1); y = firedxfired(i, 2);
         if (x == y) continue; end
         S(x,y) = (1+rewiring_rate)*S(x, y);
     end
     nonnull = sum(S~=0, "all");
-    delta_downscale = (sum(S, "all") - inc_weights_sum)/nonnull;
-    S(S~=0) = S(S~=0) - delta_downscale;
+    downscale_mute_by = (sum(S, "all") - increased_weights_sum)/nonnull;
+    S(S~=0) = S(S~=0) - downscale_mute_by;
 end
 
 
